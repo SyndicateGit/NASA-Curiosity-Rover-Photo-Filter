bfa37d0903d685658b15ffd6f376a7431434d07a
Raymond Zeng <shilei.zeng98@outlook.com>
2023-10-26 09:26:56 -0400

    Implemented flexible date format and query

--- src/index.js
+++ src/index.js
@@ -7,9 +7,9 @@ var apiKey = 'rzbEZcwqrjbwZVofGMBowwRLvaOoEmOvbg8tn8vW' // Mars API Key
 // https://api.nasa.gov/index.html for information about api
 
 // Mission Manifest:
-var name = 'Curiosity'; // Name of the Rover
-var launch_date; // The Rover's landing date on Mars
-var landing_date; // The Rover's launch date from Earth
+var name = "Curiosity"; // Name of the Rover
+var launch_date = "2011-11-26"; // The Rover's landing date on Mars
+var landing_date = "2012-08-05"; // The Rover's launch date from Earth
 var status; // The Rover's mission status
 var max_sol; // The most recent Martian sol from which photos exist
 var max_date; // The most recent Earth date from which photos exist
@@ -20,15 +20,21 @@ var total_photos; // Number of photos taken by that Rover on that sol
 var cameras; // Cameras for which there are photos by that Rover on that sol
 
 // Photo Queries 
-var queryDate = '2023-05-25' // YYYY-MM-DD format
-var querySol = '1000' // 0 to 3986 (max atm)
-var queryCamera = 'NAVCAM'; // Selection: NAVCAM, MAHLI, PANCAM, MARDI, FHAZ, RHAZ, CHEMCAM, MINITES, MAST
+var date = '2023-05-25' // YYYY-MM-DD format
+var queryEarthDate = '&earth_date=' + date;
+var sol = '1000' // 0 to 3986 (max atm)
+var querSol = '&sol=' + sol;
+
+var queryDate = queryEarthDate; // Stores which date format user chooses
+
+var camera = 'NAVCAM'; // Selection: NAVCAM, MAHLI, PANCAM, MARDI, FHAZ, RHAZ, CHEMCAM, MINITES, MAST
+var queryCamera = '&camera=' + camera
 var queryPage = '1'; // 25 items per page
 
 
-async function requestPhotos(queryDate){
+async function requestPhotos(){
   const img = document.querySelector('img');
-  const response = await fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=${queryDate}&camera=${queryCamera}&page=${queryPage}&api_key=${apiKey}`, {mode: 'cors'});
+  const response = await fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?page=${queryPage}${queryCamera}${queryDate}&api_key=${apiKey}`, {mode: 'cors'});
   
   const photosJson = await response.json();
   
